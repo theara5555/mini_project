1607016ffe64cb87930d83de6803ca380e4cd2eb
ThearaChim <chimtheara93@gmail.com>
2024-09-04 14:27:59 +0700

    Hospital

---
+++ js/index.js
@@ -0,0 +1,10 @@
+AOS.init({
+  duration: 1000, // Duration of animation
+  once: true, // Animation happens only once
+});
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1000, // Adjust the duration for the animation
+    once: true, // Ensures animation only happens once per element
+  });
+});
