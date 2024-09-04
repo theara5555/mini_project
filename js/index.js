114bf08ae5896382fa06d3180ce6686127a8b9d8
PorKeat <chhunsopheak168@gmail.com>
2024-09-04 15:41:05 +0700

    project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { card, data } from "./CardComponent.js";
+let renderCard = document.querySelector("#renderCard");
 AOS.init({
   duration: 1000, // Duration of animation
   once: true, // Animation happens only once
